7226a5e54cb253d6739d1cbfb3fcdb3f5d8819d1
Helgi Hrafn Gunnarsson <helgi@binary.is>
2018-08-16 11:00:57 +0000

    super_iter moved from main file to utils.

--- utils.py
+++ utils.py
@@ -40,3 +40,40 @@ def xml_lists_identical(one, two):
             return False
 
     return True
+
+
+# A super-iterator for containing all sorts of extra functionality that we
+# don't get with a regular Python iterator. Note that this thing is
+# incompatible with yields and is NOT a subclass of `iter` (since that's not
+# possible), but rather a class trying its best to masquerade as one.
+class super_iter():
+    def __init__(self, collection):
+        self.collection = collection
+        self.index = 0
+
+    def __next__(self):
+        try:
+            result = self.collection[self.index]
+            self.index += 1
+        except IndexError:
+            raise StopIteration
+        return result
+
+    '''
+    def prev(self):
+        self.index -= 1
+        if self.index < 0:
+            raise StopIteration
+        return self.collection[self.index]
+    '''
+
+    def __iter__(self):
+        return self
+
+    # Peek into the next item of the iterator without advancing it. Works with
+    # negative numbers to take a peek at previous items.
+    def peek(self, number_of_lines=1):
+        peek_index = self.index - 1 + number_of_lines
+        if peek_index >= len(self.collection) or peek_index < 0:
+            return None
+        return self.collection[peek_index]
