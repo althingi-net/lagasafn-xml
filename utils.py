dbd61eeceb23d3ea8fde6705e0a8ecebdee53e9a
Helgi Hrafn Gunnarsson <helgi@binary.is>
2022-10-03 01:59:05 +0000

    Improved handling of numarts going from Z to Þ.

--- utils.py
+++ utils.py
@@ -144,6 +144,10 @@ def numart_next_nrs(prev_numart):
             next_numart_nr = '(%s)' % chr(int(ord(numart_to_increment)) + 1)
             expected_numart_nrs.append(next_numart_nr)
 
+        elif prev_numart_nr == 'z':
+
+            expected_numart_nrs = 'þ'
+
         elif prev_numart_nr == 'þ':
 
             expected_numart_nrs = 'æ'
