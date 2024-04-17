ed13db56c4b37f3ae38880d4f4f5cf059af0c9cf
Helgi Hrafn Gunnarsson <helgi@binary.is>
2024-04-17 22:57:15 +0000

    Improved UI, particularly of TOC within law.

--- web/mechlaw/constants.py
+++ web/mechlaw/constants.py
@@ -1,6 +1,6 @@
 from django.utils.translation import gettext_lazy as _
 
-PROJECT_NAME = _("MechLaw")
+PROJECT_NAME = _("Lagasafn-XML")
 PROJECT_VERSION = "0.1"
 
 AVAILABLE_LEGAL_FRAMEWORKS = ["is"]
