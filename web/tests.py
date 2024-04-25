21aaf2a47333e6cf0242af13d20b04b312644744
Helgi Hrafn Gunnarsson <helgi@binary.is>
2024-04-25 15:53:12 +0000

    Web tests improved in code and performance.

--- web/tests.py
+++ web/tests.py
@@ -7,25 +7,31 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.webdriver import WebDriver
 
 
+def remove_whitespace(s):
+    """
+    Remove all kinds of whitespace
+    """
+    return re.sub(r"\s+", "", s)
+
+
 class WebTests(StaticLiveServerTestCase):
     # fixtures = ["user-data.json"]  # No database yet.
 
     def _get_law_links(self):
-        self.selenium.get(f"{self.live_server_url}/law/list/")
+        self.get_if_needed(f"{self.live_server_url}/law/list/")
 
         xml_links = self.selenium.find_elements(By.CSS_SELECTOR, "a.law-link")
         self.assertTrue(len(xml_links) > 0, "Found no laws.")
 
-        law_links = []
-        for xml_link in xml_links:
-            law_links.append(
-                {
-                    "href": xml_link.get_attribute("href"),
-                    "identifier": xml_link.get_attribute("data-identifier"),
-                    "original_url": xml_link.get_attribute("data-original-url"),
-                }
-            )
-
+        script = """
+            const links = Array.from(document.querySelectorAll("a.law-link"));
+            return links.map(link => ({
+                href: window.location.origin + link.getAttribute('href'),
+                identifier: link.getAttribute('data-identifier'),
+                original_url: link.getAttribute('data-original-url')
+            }));
+        """
+        law_links = self.selenium.execute_script(script)
         return law_links
 
     @classmethod
@@ -45,82 +51,96 @@ class WebTests(StaticLiveServerTestCase):
 
         super().tearDownClass()
 
-    def test_javascript(self):
+    def get_if_needed(self, href):
+        if self.selenium.current_url != href:
+            self.selenium.get(href)
+
+    def check_javascript(self, law_link):
         """
-        Runs through laws displayed in the web app, opens them and sees if the
-        initial JavaScript managed to complete without errors.
+        Checks to see if the currently open page ran JavaScript without errors.
         """
 
-        law_links = self._get_law_links()
+        success = False
 
-        has_errors = False
-        for law_link in law_links:
-            self.selenium.get(law_link["href"])
+        self.get_if_needed(law_link["href"])
 
-            print("Testing %s..." % law_link["identifier"], end="", flush=False)
+        try:
+            self.selenium.find_element(By.ID, "law-javascript-success")
+            self.problems.success(law_link["identifier"], "javascript")
+            success = True
 
-            try:
-                self.selenium.find_element(By.ID, "law-javascript-success")
-                self.problems.success(law_link["identifier"], "javascript")
-                print(" success")
-            except NoSuchElementException:
+        except NoSuchElementException:
 
-                # Retrieve the error message.
-                body = self.selenium.find_element(By.CSS_SELECTOR, "body")
-                message = body.get_attribute("js-error-message")
+            # Retrieve the error message.
+            body = self.selenium.find_element(By.CSS_SELECTOR, "body")
+            message = body.get_attribute("js-error-message")
 
-                self.problems.failure(law_link["identifier"], "javascript", message)
+            self.problems.failure(law_link["identifier"], "javascript", message)
 
-                print(" failure")
-                has_errors = True
+        return success
 
-        self.assertFalse(has_errors, "JavaScript errors detected.")
-
-    def test_content(self):
+    def check_content(self, law_link):
         """
         Checks if locally rendered content is identical to remote content.
         """
 
+        success = False
+
+        self.get_if_needed(law_link["href"])
+
+        # Get the text as rendered by our web application.
+        local_text = remove_whitespace(
+            self.selenium.find_element(By.CSS_SELECTOR, "law").text
+        )
+
+        # Get the official text.
+        self.get_if_needed(f"{law_link['href']}cleaned/")
+
+        remote_text = remove_whitespace(
+            self.selenium.find_element(By.CSS_SELECTOR, "body").text
+        )
+
+        # Remove known garbage from remote text.
+        garbage_denomenator = "Prentaítveimurdálkum."
+        remote_text = remote_text[
+            remote_text.find(garbage_denomenator) + len(garbage_denomenator) :
+        ]
+
+        if local_text == remote_text:
+            self.problems.success(law_link["identifier"], "content")
+            success = True
+        else:
+            ratio = Levenshtein.ratio(local_text, remote_text)
+            self.problems.failure(
+                law_link["identifier"], "content", f"Levenshtein ratio: {ratio:.8f}"
+            )
+
+        return success
+
+    def test_rendering(self):
+
         law_links = self._get_law_links()
 
+        # Tells if there are any errors in any law.
         has_errors = False
+
         for law_link in law_links:
 
-            self.selenium.get(law_link["href"])
+            print("Testing %s..." % law_link["identifier"], end="", flush=False)
 
-            print("Comparing %s..." % law_link["identifier"], end="", flush=False)
+            # List of failures to be displayed in the log.
+            failures = []
 
-            def remove_whitespace(s):
-                # Remove all kinds of whitespace
-                return re.sub(r"\s+", "", s)
+            if not self.check_javascript(law_link):
+                failures.append("javascript")
 
-            # Get the text as rendered by our web application.
-            local_text = remove_whitespace(
-                self.selenium.find_element(By.CSS_SELECTOR, "law").text
-            )
+            if not self.check_content(law_link):
+                failures.append("content")
 
-            # Get the official text.
-            self.selenium.get(f"{law_link['href']}cleaned/")
-
-            remote_text = remove_whitespace(
-                self.selenium.find_element(By.CSS_SELECTOR, "body").text
-            )
-
-            # Remove known garbage from remote text.
-            garbage_denomenator = "Prentaítveimurdálkum."
-            remote_text = remote_text[
-                remote_text.find(garbage_denomenator) + len(garbage_denomenator) :
-            ]
-
-            if local_text == remote_text:
-                self.problems.success(law_link["identifier"], "content")
-                print(" same")
+            if len(failures) == 0:
+                print(" success")
             else:
-                ratio = Levenshtein.ratio(local_text, remote_text)
-                self.problems.failure(
-                    law_link["identifier"], "content", f"Levenshtein ratio: {ratio:.8f}"
-                )
-                print(" different")
                 has_errors = True
+                print(" failure: %s" % ", ".join(failures))
 
         self.assertFalse(has_errors, "Content mismatch detected.")
