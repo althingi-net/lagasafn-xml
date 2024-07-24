b6aff4fa2c380f99bd5a4c97c7a0f529e990ac9a
Smári McCarthy <smari@ecosophy.is>
2024-07-24 15:38:49 +0000

    Better polycat

--- polycat.py
+++ polycat.py
@@ -16,6 +16,22 @@ import os
 @click.option('-d', '--dir', default='.', help='Directory to search for files')
 @click.argument('files', nargs=-1)
 def polycat(before, after, files, dir, show_filenames, show_line_numbers):
+    """
+    Print lines from files with context.
+
+    FILES is a list of filenames and line numbers in the format filename:line, separated by spaces. 
+    Trailing commas will be stripped.
+
+    You can pass -d or --dir to specify a directory to search for the files, or set the POLYCAT_DIR environment variable.
+
+    You can pass -b or --before to specify the number of lines to print before each line, and -a or --after to specify the number of lines to print after each line.
+
+    You can pass -f or --show-filenames to show the filename before each line, and -n or --show-line-numbers to show the line number before each line.
+    """
+
+    if dir == '.' and os.getenv('POLYCAT_DIR'):
+            dir = os.getenv('POLYCAT_DIR')
+
     for file in files:
         filename, line = file.split(':')
         line = line.rstrip(',')     # Strip trailing commas if present
