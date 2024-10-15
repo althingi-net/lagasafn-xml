fed57689fa5a1b6ce5df984f5475018d36004503
Smári McCarthy <smari@ecosophy.is>
2024-10-15 09:05:54 -0700

    Abstract ElasticSearch connection creation. Add ability to define mappings.

--- gensearch.py
+++ gensearch.py
@@ -42,7 +42,7 @@ def generate_json(xml_dir: str, json_dir: str):
             root = tree.getroot()
 
             # Convert the XML to JSON
-            structure = xmljson.abdera.data(root)
+            structure = xmljson.abdera.data(root, int_type=str)
 
             open(json_dir + file.replace('.xml', '.json'), 'w').write(json.dumps(structure, indent=2))
         except ET.ParseError as e:
@@ -52,15 +52,11 @@ def generate_json(xml_dir: str, json_dir: str):
     print("JSON generated successfully")
 
 
-def update_elasticsearch(json_dir: str, elastic_server: str, elastic_index: str, elastic_apikey: str, elastic_user: str, elastic_password: str):
+def get_elasticsearch(elastic_server: str, elastic_apikey: str, elastic_user: str, elastic_password: str) -> Elasticsearch:
     if not elastic_server or not elastic_index:
         print("ElasticSearch server or index not provided. Exiting...")
-        return
-    
-    if not os.path.exists(json_dir):
-        print(f"JSON directory {json_dir} does not exist. Exiting...")
-        return
-        
+        return None
+
     authtype = None
     if elastic_apikey:
         authtype = "apikey"
@@ -68,61 +64,96 @@ def update_elasticsearch(json_dir: str, elastic_server: str, elastic_index: str,
         authtype = "basic"
     else:
         print("No authentication provided. Exiting...")
-        return
-    
-    print(f"Inserting JSON into ElasticSearch server {elastic_server}/{elastic_index} with {authtype} authentication")
+        return None
+
+    print(f"Connecting to elasticSearch server {elastic_server} with {authtype} authentication...")
 
     try:
-        # Insert or update the JSON into ElasticSearch.
-        if authtype == "basic":
+        # Retrieve the JSON from ElasticSearch
+        if authtype == "apikey":
             es = Elasticsearch(elastic_server, api_key=elastic_apikey)
-        elif authtype == "apikey":
+        elif authtype == "basic":
             es = Elasticsearch(elastic_server, basic_auth=(elastic_user, elastic_password))
-
-        files = os.listdir(json_dir)
-
-        for file in files:
-            data = json.load(open(json_dir + file))
-
-            id = file.replace('.json', '')
-
-            # Insert the data into ElasticSearch
-            es.index(index=elastic_index, id=id, document=data)
 
     except ConnectionRefusedError:
         print("Could not connect to ElasticSearch server. Exiting...")
-        return
+        return None
     except ConnectionAbortedError:
         print("Connection to ElasticSearch server aborted. Exiting...")
-        return
+        return None
     except ConnectionError as e:
         print(f"Error connecting to ElasticSearch server: {e}. Exiting...")
-        return
+        return None
     except AuthenticationException as e:
         print(f"Error authenticating to ElasticSearch server: {e}. Exiting...")
-        return
+        return None
     except AuthenticationException as e:
         print(f"Error authorizing to ElasticSearch server: {e}. Exiting...")
+        return None
+    
+    info = es.info()
+    print(f"Connected to ElasticSearch server {info['name']} version {info['version']['number']}")
+    
+    return es
+
+
+def set_elasticsearch_mapping(es: Elasticsearch, index: str, mapping: dict):
+    print(f"Setting mapping for index {index}")
+
+    try:
+        es.indices.create(index=index, body=mapping)
+    except Exception as e:
+        print(f"Error setting mapping for index {index}: {e}. Exiting...")
         return
 
-    
+    print(f"Mapping set for index {index}")
+
+
+def update_elasticsearch(json_dir: str, es: Elasticsearch, elastic_index: str):    
+    if not os.path.exists(json_dir):
+        print(f"JSON directory {json_dir} does not exist. Exiting...")
+        return
+        
+    files = os.listdir(json_dir)
+    count = len(files)
+    idx = 0
+
+    for file in files:
+        idx += 1
+        print(f"{idx:<4}/{count:<4}: {file:<20}", end='\r')
+
+        data = json.load(open(json_dir + file))
+        id = file.replace('.json', '')
+
+        # Insert the data into ElasticSearch
+        es.index(index=elastic_index, id=id, document=data)
+
 
 @click.command()
 @click.option('--xml-dir',                default='data/xml/',  help='Directory containing the XML files')
 @click.option('--json-dir',               default='data/json/', help='Directory to store/retrieve the JSON files')
 @click.option('--generate/--no-generate', default=True,         help='Generate JSON from the XML')
 @click.option('--update/--no-update',     default=True,         help='Insert the JSON into ElasticSearch')
+@click.option('--add-mapping',            type=click.File,      help='Add mapping to ElasticSearch')
 @click.option('--elastic-server',         default=lambda: os.environ.get('ELASTIC_SERVER', 'https://localhost:9200'), help='ElasticSearch server')
 @click.option('--elastic-index',          default=lambda: os.environ.get('ELASTIC_INDEX'), help='ElasticSearch index')
 @click.option('--elastic-apikey',         default=lambda: os.environ.get('ELASTIC_APIKEY'), help='ElasticSearch API Key')
 @click.option('--elastic-user',           default=lambda: os.environ.get('ELASTIC_USER'), help='ElasticSearch user')
 @click.option('--elastic-password',       default=lambda: os.environ.get('ELASTIC_PASSWORD'), help='ElasticSearch password')
-def main(xml_dir: str, json_dir: str, generate: bool, update: bool, elastic_server: str, elastic_index: str, elastic_apikey: str, elastic_user: str, elastic_password: str):    
+def main(xml_dir: str, json_dir: str, generate: bool, update: bool, add_mapping: click.File, 
+         elastic_server: str, elastic_index: str, elastic_apikey: str, elastic_user: str, elastic_password: str
+        ):
     if generate:
         generate_json(xml_dir, json_dir)
 
-    if update:
-        update_elasticsearch(json_dir, elastic_server, elastic_index, elastic_apikey, elastic_user, elastic_password)
+    if add_mapping or update:
+        es = get_elasticsearch(elastic_server, elastic_apikey, elastic_user, elastic_password)
+
+        if add_mapping:
+            set_elasticsearch_mapping(es, elastic_index, json.load(add_mapping))
+
+        if update:
+            update_elasticsearch(json_dir, es, elastic_index)
 
 
 if __name__ == '__main__':
