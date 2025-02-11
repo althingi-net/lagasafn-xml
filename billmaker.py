58253d1a15e0c8f1c844d7bb62ba173f566ff3cb
Helgi Hrafn Gunnarsson <helgi@binary.is>
2025-02-11 11:39:02 +0000

    Billmaker: XML output normalized and minor refactoring.

--- billmaker.py
+++ billmaker.py
@@ -1,8 +1,8 @@
-from lagasafn import rted
-from lxml import etree
-
+#!/usr/bin/env python3
 import click
-
+from lagasafn import rted
+from lagasafn.utils import write_xml
+from lxml import etree
 
 
 @click.command()
@@ -10,15 +10,12 @@ import click
 @click.argument('target', type=click.Path(exists=True))
 @click.option('--output', '-o', default='output.xml', help='Output file')
 def make_bill(origin, target, output):
-    #file1 = "data/xml/154a/2003.83.xml"
-    #file2 = "data/xml/154b/2003.83.xml"
     tree1 = etree.parse(origin)
     tree2 = etree.parse(target)
 
     bill = rted.make_bill(tree1, tree2)
 
-    with open(output, 'wb') as f:
-        f.write(etree.tostring(bill, pretty_print=True, encoding="utf-8"))
+    write_xml(bill, output)
     print("Wrote bill to", output)
 
 if __name__ == '__main__':
