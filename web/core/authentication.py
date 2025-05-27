c863bb2bdca3c21a75c58b2a7a967e00ca3a6aec
Helgi Hrafn Gunnarsson <helgihg@users.noreply.github.com>
2025-05-27 10:13:31 +0000

    Check against API_ACCESS_TOKEN rather than literal string.

--- web/core/authentication.py
+++ web/core/authentication.py
@@ -6,5 +6,5 @@ class APIAuthentication(HttpBearer):
     Validates incoming API requests using access token.
     """
     def authenticate(self, request, token):
-        if token == "supersecret":
+        if token == API_ACCESS_TOKEN:
             return token
