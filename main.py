5911614d5986f729087c3377ee3794b4777fd391
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2025-06-11 12:21:08 +0700

    Re-create project using uv package manager

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from edithing-python!")
-
-
-if __name__ == "__main__":
-    main()
--- web/api.py
+++
@@ -1,7 +0,0 @@
-from ninja import NinjaAPI
-
-api = NinjaAPI()
-
-@api.get("/hello")
-def hello(request, name: str | int):
-    return {"message": f"Hello, {name}!"}
