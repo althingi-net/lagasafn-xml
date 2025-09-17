33814b0808d68380bbe2a7ca9569a5aa028ff62e
Helgi Hrafn Gunnarsson <helgi@binary.is>
2025-09-17 00:15:51 +0000

    API: CORS implemented.

--- codex-api/mechlaw/settings.py
+++ codex-api/mechlaw/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "bill",
     "core",
     "law",
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -124,6 +126,11 @@ CACHES = {
         }
     }
 }
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 
 
 from mechlaw.local_settings import *
