efc0129f73b3f8cb7d8c7daedb1684dabb059122
Gauti Nils Bernhardsson <gautinils@gmail.com>
2025-12-11 17:00:04 +0000

    feat: advert api

---
+++ codex-ui/src/api/models/AdvertIndexInfo.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type AdvertIndexInfo = {
+    codex_version?: string;
+    date_from?: string;
+    date_to?: string;
+    total_count?: number;
+};
+
